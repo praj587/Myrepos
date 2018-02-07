56a3c6c4e451868a40919544310a49c996294d45
praj587 <parthibanraj@live.com>
2018-02-07 09:59:13 -0500

    inital commit
    Initial commit

---
+++ POS Go/POC Go/Models/Model1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace POC_Go.Models
+{
+    public class Model1
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ POS Go/POC Go/obj/Release/android/src/md53d2801bc2168f5764389c7f74e9e849d/MainActivity.java
@@ -0,0 +1,47 @@
+package md53d2801bc2168f5764389c7f74e9e849d;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("POC_Go.MainActivity, POC Go, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("POC_Go.MainActivity, POC Go, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
