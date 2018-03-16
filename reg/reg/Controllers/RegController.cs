04742b2a9174195d74681fbbeda09ef9c8cd2aa8
praj587 <parthibanraj@live.com>
2018-03-16 12:26:09 -0400

    parveen

---
+++ reg/reg/Controllers/RegController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using reg.Models;
+
+namespace reg.Controllers
+{
+    public class RegController : Controller
+    {
+        // GET: Reg
+        public ActionResult Index()
+        {
+           
+
+
+            return View();
+        }
+        [ValidateInput(true)]
+        [HttpPost]
+        public ActionResult Register(tbl_Reg regdata)
+        {
+            if (!ModelState.IsValid)
+            {
+                //var Mobileno = Convert.ToString(Request["txtAmount"].ToString());
+                using (var context = new regmodelcontext())
+                {
+                    var user = context.regusers.Where(u => u.Cus_Mobile == regdata.Cus_Mobile).FirstOrDefault();
+
+                    if (user == null)
+                        return View("Success");
+                    else
+                    {
+                        return View("Failure");
+                    }
+                }
+            }
+            return View("Success");
+        }
+        public ViewResult Success()
+        {
+            return View();
+        }
+
+        public ActionResult Failure()
+        {
+            return View();
+        }
+    }
+}
---
+++ reg/reg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(reg.Startup))]
+namespace reg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
