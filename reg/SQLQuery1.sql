9786284febf7b91372bbe23f2950704ad2c4240d
praj587 <parthibanraj@live.com>
2018-03-19 09:16:52 -0400

    ttt

--- reg/SQLQuery1.sql
+++
@@ -1,4 +0,0 @@
-﻿select * from tbl_Reg
---Create table tbl_Reg(Cus_Id int identity(1,1) not null, Cus_Name varchar(150) not null,Cus_Mobile varchar(15),Cus_Services varchar(150))
-
---alter table tbl_Reg add Cus_Company varchar(100)
--- reg/reg/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="reg.MvcApplication" Language="C#" %>
